216031ab37a7d3db7247692731158a89cb2cb307
prnvthir14 <prnvthir@gmail.com>
2020-12-12 00:47:15 +0000

    odd checker to help post office

---
+++ odd.js
@@ -0,0 +1,20 @@
+//odds checker
+
+
+let number = 13;
+
+const isOdd = function (number) {
+	if (number % 2 === 1){
+	return true;	
+	}else {
+	return false;
+	
+	}
+
+
+}
+
+
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
